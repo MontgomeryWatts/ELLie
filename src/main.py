94baad70b4944030b5867e1553b095af3869041a
Joshua Karns <jkarns275@gmail.com>
2019-07-24 23:03:11 -0400

    fort chin

--- src/main.py
+++ src/main.py
@@ -1,10 +1,27 @@
 import discord
 import os
+import fortune
+
+
 
 class ELLie(discord.Client):
+
+	FORTUNE_TELLER = fortune.FortuneTeller('res/fortunes')
+
+	def handle_command(command):
+		tokens = command.split(' ')
+
+		if tokens[0] == 'fortune':
+			return ELLie.FORTUNE_TELLER.tell()
+		else:
+			return "No such command."
+
 	async def on_message(self, message):
 		if message.content == 'ELL':
 			await message.add_reaction('📠')
+		elif message.content.startswith(':'):
+			res = ELLie.handle_command(message.content[1:].lstrip().rstrip())
+			await message.channel.send(res)
 
 ellie = ELLie()
 ellie.run(os.environ['DISCORD_TOKEN'])
