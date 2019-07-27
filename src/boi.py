f2b226f3cc12a85c5acb280589c9f502ff524d5c
Joshua Karns <jkarns275@gmail.com>
2019-07-26 23:46:21 -0400

    Added to string functions. Changed Var class to Id

--- src/boi.py
+++ src/boi.py
@@ -1,5 +1,21 @@
 from typing import Any, List, Tuple, Optional, Union
 
+class Span:
+
+    """
+    a slice of the input string
+    """
+
+    EMPTY = None
+
+    def __init__(self, start: int, end: int, input: str):
+        self.start = start
+        self.end = end
+        self.slice = input[start:end]
+        self.input = input
+
+Span.EMPTY = Span(0, 0, "")
+
 class InterpreterException(Exception):
 
     def __init__(self, reason: str, span: 'Span' = None):
@@ -12,7 +28,6 @@ class InterpreterException(Exception):
         return f"Interpreter Error: {self.reason}.\n" + \
                 "note: encountered error here\n" + \
                 self.span.slice
-
 
 class Context:
 
@@ -31,28 +46,28 @@ class Context:
 
         return self.stack_frames[-1]
 
-    def push_args(self, args: List['Var'], values: List['Value']):
+    def push_args(self, args: List['Id'], values: List['Value']):
         assert len(args) == len(values)
 
         for arg, value in zip(args, values):
             self.push_var(arg, value)
 
-    def pop_args(self, args: List['Var']):
+    def pop_args(self, args: List['Id']):
         return list(map(lambda arg: self.pop_var(arg), args))
 
-    def push_var(self, var: 'Var', value: 'Value'):
+    def push_var(self, var: 'Id', value: 'Value'):
         sf = self.current_stack_frame()
 
-        if var.name in sf:
+        if var in sf:
             raise InterpreterException("name shadowing is not supported")
         else:
-            sf[var.name] = value
+            sf[var] = value
     
-    def pop_var(self, var: 'Var') -> 'Value':
+    def pop_var(self, var: 'Id') -> 'Value':
         sf = self.current_stack_frame()
 
-        if var.name in sf:
-            return sf.pop(var.name)
+        if var in sf:
+            return sf.pop(var)
         else:
             raise InterpreterException("tried to pop var which hasn't been added to the current stack frame", var.span)
 
@@ -61,19 +76,19 @@ class Context:
 
         argn = len(fn.args)
 
-        if (fn.name.name, argn) in sf:
-            raise InterpreterException(f"function '{fn.name.name}' with {argn} arguments is already defined, and name shadowing is not supported", fn.span)
+        if (fn.name, argn) in sf:
+            raise InterpreterException(f"function '{fn.name}' with {argn} arguments is already defined, and name shadowing is not supported", fn.span)
         else:
-            sf[(fn.name.name, argn)] = fn
+            sf[(fn.name, argn)] = fn
 
     def pop_function(self, fn: 'Function'):
         sf = self.current_stack_frame()
         argn = len(fn.args)
 
-        if (fn.name.name, argn) in sf:
-            return sf.pop((fn.name.name, argn))
+        if (fn.name, argn) in sf:
+            return sf.pop((fn.name, argn))
         else:
-            raise InterpreterException(f"tried top pop function '{fn.name.name}' with {argn} arguments which hasn't been added to the current stack frame", fn.span)
+            raise InterpreterException(f"tried top pop function '{fn.name}' with {argn} arguments which hasn't been added to the current stack frame", fn.span)
 
     def get_stack_frame(self, offset):
         assert offset <= 0
@@ -84,45 +99,34 @@ class Context:
         return self.stack_frames[offset]
 
 
-    def get_function(self, var: 'Var', argn: int):
+    def get_function(self, var: 'Id', argn: int):
         assert argn >= 0
 
         sf = self.current_stack_frame()
         offset = -1
         
         while sf is not None:
-            if (var.name, argn) in sf:
-                return sf[(var.name, argn)]
+            if (var, argn) in sf:
+                return sf[(var, argn)]
 
             sf = self.get_stack_frame(offset)
             offset -= 1
         
-        raise InterpreterException(f"no such function '{var.name}' with {argn} arguments", var.span)
+        raise InterpreterException(f"no such function '{var}' with {argn} arguments", var.span)
 
-    def get_var(self, var: 'Var'):
+    def get_var(self, var: 'Id'):
 
         sf = self.current_stack_frame()
         offset = -1
         
         while sf is not None:
-            if var.name in sf:
-                return sf[var.name]
+            if var in sf:
+                return sf[var]
 
             sf = self.get_stack_frame(offset)
             offset -= 1
         
-        raise InterpreterException(f"no variable with name '{var.name}'", var.span)
-
-class Span:
-
-    EMPTY = None
-
-    def __init__(self, start, end, slice):
-        self.start = start
-        self.end = end
-        self.slice = slice
-
-Span.EMPTY = Span(0, 0, "")
+        raise InterpreterException(f"no variable with name '{var}'", var.span)
 
 class AST:
 
@@ -140,11 +144,37 @@ class Expr(AST):
     def __init__(self, span: Span):
         super().__init__(span)
 
+class Id(AST):
+
+    def __init__(self, id: str, span: Span):
+        super().__init__(span)
+
+        self.id = id
+
+
+    def eval(self, context: Context) -> 'Value':
+        return context.get_var(self)
+
+    def __str__(self):
+        return self.id
+
+    def __eq__(self, other):
+        return self.id == other.id
+    
+    def __ne__(self, other):
+        return self.id != other.id
+
+    def __hash__(self):
+        return hash(self.id)
+
 class Value(Expr):
     
     def __init__(self, value: float, span: Span):
         super().__init__(span)
         self.value = value
+
+    def __str__(self):
+        return str(self.value)
 
     def eval(self, _context: Context) -> 'Value':
         return self
@@ -185,22 +215,6 @@ class Value(Expr):
     def __ne__(self, other):
         return self.value != other.value
     
-
-
-class Var(Expr):
-
-    def __init__(self, name: str, span: Span):
-        super().__init__(span)
-
-        self.name = name
-        self.span = span
-
-    def eval(self, context: Context) -> Value:
-        return context.get_var(self)
-
-    def __str__(self):
-        return self.name
-
 class AdditiveExpr(Expr):
 
     ADDITION    = 0
@@ -214,6 +228,12 @@ class AdditiveExpr(Expr):
         self.lhs = lhs
         self.operator = operator
         self.rhs = rhs
+
+    def __str__(self):
+        if self.operator == 0:
+            return f"{self.lhs} + {self.rhs}"
+        else:
+            return f"{self.lhs} - {self.rhs}"
 
     def eval(self, context: Context) -> Value:
         lhs_value = self.lhs.eval(context)
@@ -233,6 +253,9 @@ class ConditionExpr(Expr):
         super().__init__(span)
 
         self.expr = condition
+
+    def __str__(self):
+        return str(self.expr)
 
     def eval(self, context: Context) -> bool:
         return self.expr.eval(context).value not in ConditionExpr.FALSE_VALUES
@@ -256,6 +279,15 @@ class ComparisonExpr(Expr):
         NEQ:    lambda l, r: l != r
     }
 
+    COMPARISON_OP_TO_STR = {
+        GT:     ">",
+        GTE:    ">=",
+        LT:     "<",
+        LTE:    "<=",
+        EQ:     "=",
+        NEQ:    "<>"
+    }
+
     def __init__(self, lhs: Expr, operator: int, rhs: Expr, span: Span):
         super().__init__(span)
 
@@ -264,6 +296,9 @@ class ComparisonExpr(Expr):
         self.lhs = lhs
         self.rhs = rhs
         self.operator = operator
+
+    def __str__(self):
+        return f"{self.lhs} + {ComparisonExpr.COMPARISON_OP_TO_STR[self.operator]} + {self.rhs}"
 
     def eval(self, context: Context):
         return ComparisonExpr.COMPARISON_FNS[self.operator](self.lhs.eval(context), self.rhs.eval(context))
@@ -281,7 +316,7 @@ class LambdaExpr(Expr):
     global scope, lambdas are nested within expressions.
     """
 
-    def __init__(self, name: str, args: List[Var], value: Expr, body: Expr, span: Span):
+    def __init__(self, name: Id, args: List[Id], value: Expr, body: Expr, span: Span):
         super().__init__(span)
 
         self.name = name
@@ -289,8 +324,13 @@ class LambdaExpr(Expr):
         self.value = value
         self.body = body
 
-        self.as_fn = Function(Var(name, span), args, value, span, is_lambda=True)
+        self.as_fn = Function(name, args, value, span, is_lambda=True)
     
+    def __str__(self):
+        import re
+        args = re.sub('[\\[\\],]', '', str(self.args))
+        return f"let {self.name} {args} = {self.value} in \n    {self.body}"
+
     def eval(self, context: Context) -> Value:
         context.push_function(self.as_fn)
 
@@ -309,7 +349,7 @@ class LetExpr(Expr):
 
     """
 
-    def __init__(self, name: Var, value: Expr, body: Expr, span: Span):
+    def __init__(self, name: Id, value: Expr, body: Expr, span: Span):
         super().__init__(span)
 
         self.name = name
@@ -338,7 +378,7 @@ class FunctionCall(Expr):
 
     """
 
-    def __init__(self, name: Var, argv: List[Expr], span: Span):
+    def __init__(self, name: Id, argv: List[Expr], span: Span):
         super().__init__(span)
 
         self.name = name
@@ -383,16 +423,16 @@ class Function(AST):
     """
     GRAMMAR:
 
-    let ~ <name: id> ~ ( <unit> | <args: id+> ) ~ = ~ <value: expr> ~ in ~ <body: expr>
+    let ~ <name: id> ~ ( <unit> | <args: id+> ) ~ = ~ <value: expr>
 
     """
 
-    def __init__(self, name: Var, args: List[Var], body: Expr, span: Span, is_lambda: bool = False):
+    def __init__(self, name: Id, args: List[Id], value: Expr, span: Span, is_lambda: bool = False):
         super().__init__(span)
 
         self.name = name
         self.args = args
-        self.body = body
+        self.value = value
         self.is_lambda = is_lambda
 
         self.argv = None
@@ -403,6 +443,11 @@ class Function(AST):
         
         self.argv = argv
         return True
+
+    def __str__(self):
+        import re
+        args = re.sub('[\\[\\],]', '', str(self.args))
+        return f"let {self.name} {args} = {self.value}"
 
     def eval(self, context: Context) -> Value:
         """
@@ -416,7 +461,7 @@ class Function(AST):
 
         context.push_args(self.args, self.argv)
         
-        r = self.body.eval(context)
+        r = self.value.eval(context)
         
         self.argv = None
 
@@ -448,32 +493,41 @@ class Program:
 def success(test_name):
     print(f"\n ╠═ {test_name + ' test':34} :: Okay", end='')
 
+def test_if_expr():
+    test_true_expr = IfExpr(ConditionExpr(Value(4.0, Span.EMPTY), Span.EMPTY), Value(1.0, Span.EMPTY), Value(0.0, Span.EMPTY), Span.EMPTY)
+    test_false_expr = IfExpr(ConditionExpr(Value(0.0, Span.EMPTY), Span.EMPTY), Value(1.0, Span.EMPTY), Value(0.0, Span.EMPTY), Span.EMPTY)
+
+    assert test_true_expr.eval(Context()).value == 1.0
+    assert test_false_expr.eval(Context()).value == 0.0
+
+    success("if_expr")
+
 def test_let_expr():
-    let_expr = LetExpr(Var("a", Span.EMPTY), Value(5.0, Span.EMPTY), Var("a", Span.EMPTY), Span.EMPTY)
+    let_expr = LetExpr(Id("a", Span.EMPTY), Value(5.0, Span.EMPTY), Id("a", Span.EMPTY), Span.EMPTY)
 
     assert let_expr.eval(Context()).value == 5.0
 
     success("let_expr")
 
 def test_simple_lambda_expr():
-    lambda_expr = LambdaExpr("lambda", [Var("a", Span.EMPTY), Var("b", Span.EMPTY)], AdditiveExpr(Var("a", Span.EMPTY), AdditiveExpr.ADDITION, Var("b", Span.EMPTY), Span.EMPTY),
-                                FunctionCall(Var("lambda", Span.EMPTY), [Value(4.0, Span.EMPTY), Value(1.0, Span.EMPTY)], Span.EMPTY), Span.EMPTY)
+    lambda_expr = LambdaExpr(Id("lambda", Span.EMPTY), [Id("a", Span.EMPTY), Id("b", Span.EMPTY)], AdditiveExpr(Id("a", Span.EMPTY), AdditiveExpr.ADDITION, Id("b", Span.EMPTY), Span.EMPTY),
+                                FunctionCall(Id("lambda", Span.EMPTY), [Value(4.0, Span.EMPTY), Value(1.0, Span.EMPTY)], Span.EMPTY), Span.EMPTY)
 
     assert lambda_expr.eval(Context()).value == 5.0
 
     success("simple_lambda_expr")
 
-def test_simple_function():
-    function_ast = Function(Var("sum", Span.EMPTY), [Var("a", Span.EMPTY), Var("b", Span.EMPTY)], AdditiveExpr(Var("a", Span.EMPTY), AdditiveExpr.ADDITION, Var("b", Span.EMPTY), Span.EMPTY), Span.EMPTY)
+def test_simple_function_call():
+    function_ast = Function(Id("sum", Span.EMPTY), [Id("a", Span.EMPTY), Id("b", Span.EMPTY)], AdditiveExpr(Id("a", Span.EMPTY), AdditiveExpr.ADDITION, Id("b", Span.EMPTY), Span.EMPTY), Span.EMPTY)
     
     program = Program([function_ast])
     program.run()
 
-    function_call_ast = FunctionCall(Var("sum", Span.EMPTY), [Value(4.0, Span.EMPTY), Value(5.0, Span.EMPTY)], Span.EMPTY)
+    function_call_ast = FunctionCall(Id("sum", Span.EMPTY), [Value(4.0, Span.EMPTY), Value(5.0, Span.EMPTY)], Span.EMPTY)
 
     assert function_call_ast.eval(program.context).value == 9
 
-    success("simple_function")
+    success("simple_function_call")
 
 def test_additive_expr():
     context = Context()
